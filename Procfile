c545c1823bdcc19c1b3d12be3229104ace463a5b
aliza <alizaahsan546@gmail.com>
2026-01-24 00:42:52 +0500

    Initial host-ready commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: pip install -r backend/requirements.txt && python backend/app.py
+web: sh start.sh
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export FLASK_APP=backend/app.py
+export FLASK_ENV=production
+python backend/app.py
